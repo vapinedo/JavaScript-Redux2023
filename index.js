83aa645570211b37a970b97a20dcd4b75326c5d1
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-08-21 11:00:25 -0500

    11 - Multiple Reducers

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const redux = require("redux");
  
 const CAKE_ORDER = "CAKE_ORDER";
 const CAKE_RESTOCK = "CAKE_RESTOCK";
+const ICECREAM_ORDER = "ICECREAM_ORDER";
+const ICECREAM_RESTOCK = "ICECREAM_RESTOCK";
 
 const orderCake = () => {
     return {
@@ -15,12 +17,32 @@ const restockCake = (quantity = 1) => {
         payload: quantity
     }
 };
-
-const initialState = {
-    numberOfCakes: 10
+const orderIceCream = () => {
+    return {
+        type: ICECREAM_ORDER,
+        payload: 1
+    }
+};
+const restockIceCream = (quantity = 1) => {
+    return {
+        type: ICECREAM_RESTOCK,
+        payload: quantity
+    }
 };
 
-const reducer = (state = initialState, action) => {
+// const initialState = {
+//     numberOfCakes: 10,
+//     numberOfIceCreams: 20
+// };
+
+const initialCakeState = {
+    numberOfIceCreams: 10
+};
+const initialIcreCreamState = {
+    numberOfIceCreams: 20
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case CAKE_ORDER:
             return {
@@ -31,6 +53,21 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 numberOfCakes: state.numberOfCakes + action.payload
+            }
+        default: return state;
+    }
+};
+const iceCreamReducer = (state = initialIcreCreamState, action) => {
+    switch(action.type) {
+        case ICECREAM_ORDER:
+            return {
+                ...state,
+                numberOfIceCreams: state.numberOfIceCreams - 1
+            }
+        case ICECREAM_RESTOCK:
+            return {
+                ...state,
+                numberOfIceCreams: state.numberOfIceCreams + action.payload
             }
         default: return state;
     }
@@ -47,12 +84,20 @@ const unsubscribe = store.subscribe(() => console.log("Updated state", store.get
 
 const actionCreators = {
     orderCake,
-    restockCake
+    restockCake,
+    orderIceCream,
+    restockIceCream
 };
 const actions = redux.bindActionCreators(actionCreators, store.dispatch);
 actions.orderCake();
 actions.orderCake();
 actions.orderCake();
 actions.restockCake(3);
+
+console.log("-----");
+
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
 
 unsubscribe();
