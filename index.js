6602b6b8fab5240014ca7316f9baabe5a51462e1
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-08-21 09:30:13 -0500

    8 - Restocking Cakes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const redux = require("redux");
  
 const CAKE_ORDER = "CAKE_ORDER";
+const CAKE_RESTOCK = "CAKE_RESTOCK";
 
 const orderCake = () => {
     return {
         type: CAKE_ORDER,
-        quantity: 1
+        payload: 1
+    }
+};
+const restockCake = (quantity = 1) => {
+    return {
+        type: CAKE_RESTOCK,
+        payload: quantity
     }
 };
 
@@ -20,6 +27,11 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numberOfCakes: state.numberOfCakes - 1
             }
+        case CAKE_RESTOCK:
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes + action.payload
+            }
         default: return state;
     }
 };
@@ -31,5 +43,7 @@ const unsubscribe = store.subscribe(() => console.log("Updated state", store.get
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+
+store.dispatch(restockCake(3));
 
 unsubscribe();
